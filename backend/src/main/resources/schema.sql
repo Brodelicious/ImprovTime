53021d369568ae39d96022fd38002ce16419cecd
Bobby Richter <robert.richter@revature.net>
2021-01-25 14:32:13 -0500

    added schema

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -1,38 +1,18 @@
-drop table if exists account_type cascade;
-drop table if exists account_status cascade;
-drop table if exists roles cascade;
-drop table if exists users cascade;
-drop table if exists account cascade;
+drop table if exists improv_character cascade;
+drop table if exists setting cascade;
+drop table if exists activity cascade;
 
-create table account_type(
-	id serial primary key,
-	type_name varchar unique not null
-);
-
-create table account_status(
+create table improv_character(
 	id serial primary key,
 	name varchar unique not null
 );
 
-create table roles(
+create table setting(
 	id serial primary key,
 	name varchar unique not null
 );
 
-create table users(
+create table activity(
 	id serial primary key,
-	username varchar unique not null,
-	passwd varchar not null,
-	first_name varchar not null,
-	last_name varchar not null,
-	email varchar not null,
-	role_id integer references roles
-);
-
-create table account(
-	id serial primary key,
-	balance double,
-    status_id integer references account_status,
-	type_id integer references account_type,
-	owner_id integer references users
+	name varchar unique not null
 );
