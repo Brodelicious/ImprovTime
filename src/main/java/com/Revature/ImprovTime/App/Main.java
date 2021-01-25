a36c1f2494767769a9eec583cce9f23fec5f3e3e
Jason Zelonka <jzelonka@ufl.edu>
2021-01-25 12:43:18 -0500

    startup aquired

---
+++ src/main/java/com/Revature/ImprovTime/App/Main.java
@@ -0,0 +1,10 @@
+package com.Revature.ImprovTime.App;
+
+public class Main {
+
+	public static void run()
+	{
+		System.out.println("I have begun!");
+	}
+	
+}
--- src/main/java/com/Revature/ImprovTime/ImprovTimeApplication.java
+++ src/main/java/com/Revature/ImprovTime/ImprovTimeApplication.java
@@ -3,11 +3,14 @@ package com.Revature.ImprovTime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.Revature.ImprovTime.App.Main;
+
 @SpringBootApplication
 public class ImprovTimeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ImprovTimeApplication.class, args);
+		Main.run();
 	}
 
 }
