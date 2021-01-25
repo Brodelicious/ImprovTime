f7bb46480f436b65237fff97ee94f0d43c3be9ea
bhufnagel <Brodinator@live.com>
2021-01-25 12:02:35 -0500

    Spring Boot

---
+++ src/test/java/com/Revature/ImprovTime/ImprovTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Revature.ImprovTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImprovTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
