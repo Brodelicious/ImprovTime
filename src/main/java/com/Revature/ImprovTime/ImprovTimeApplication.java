36928ec10a339d17ffb64894f10562eeb7e58fcb
Jason Zelonka <jzelonka@ufl.edu>
2021-01-25 12:44:33 -0500

    comment run

--- src/main/java/com/Revature/ImprovTime/ImprovTimeApplication.java
+++ src/main/java/com/Revature/ImprovTime/ImprovTimeApplication.java
@@ -9,7 +9,7 @@ import com.Revature.ImprovTime.App.Main;
 public class ImprovTimeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ImprovTimeApplication.class, args);
+		//SpringApplication.run(ImprovTimeApplication.class, args);
 		Main.run();
 	}
 
