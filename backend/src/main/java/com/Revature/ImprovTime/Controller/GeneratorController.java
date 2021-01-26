dae500e5dec1209896cf32a4f9d0782993e69013
Bobby <obbes240@gmail.com>
2021-01-26 12:30:36 -0500

    Update GeneratorController.java

--- backend/src/main/java/com/Revature/ImprovTime/Controller/GeneratorController.java
+++ backend/src/main/java/com/Revature/ImprovTime/Controller/GeneratorController.java
@@ -1,5 +1,19 @@
 package com.Revature.ImprovTime.Controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/generators")
 public class GeneratorController {
 
+	@GetMapping
+	public ResponseEntity<T> getScene(){
+		
+		return ResponseEntity<T>.
+	}
+	
 }
