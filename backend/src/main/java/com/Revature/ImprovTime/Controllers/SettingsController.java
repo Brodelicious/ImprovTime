fe6037c1e5f440d74ff1be3b9036dfce14787413
Jason Zelonka <jzelonka@ufl.edu>
2021-01-26 12:11:21 -0500

    Create SettingsController.java

---
+++ backend/src/main/java/com/Revature/ImprovTime/Controllers/SettingsController.java
@@ -0,0 +1,10 @@
+package com.Revature.ImprovTime.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/setting")
+public class SettingsController {
+
+}
