88cf6551685033c31c56bb7b5297c0f42ecf3e65
Jason Zelonka <jzelonka@ufl.edu>
2021-01-27 13:49:56 -0500

    fixed h2

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,31 @@
+
+
+--Characters Table
+insert into improv_character values (default, 'All the Ninja Turtles');
+insert into improv_character values (default, 'Shredder');
+insert into improv_character values (default, 'Splinter');
+insert into improv_character values (default, 'Joe Shmo');
+insert into improv_character values (default, 'Tree #721');
+insert into improv_character values (default, 'Megameme');
+insert into improv_character values (default, 'Not so Superman');
+insert into improv_character values (default, 'A Tree Frog');
+insert into improv_character values (default, 'Romeo');
+insert into improv_character values (default, 'Doomguy');
+insert into improv_character values (default, 'Mr Incredible');
+insert into improv_character values (default, 'Rango the Chameleon');
+
+--Activity Table
+insert into activity values(default, 'mugging a shark');
+insert into activity values(default, 'milking a cow');
+insert into activity values(default, 'eating dinner with batchmates');
+insert into activity values(default, 'riding a tandem bike');
+insert into activity values(default, 'shopping for walmart goods');
+insert into activity values(default, 'looking for a spoon');
+
+--Setting Table
+insert into setting values (default, 'on an elevator ride to the 34th floor, but each floor button is lit.');
+insert into setting values (default, 'skydiving with no memory of how anyone got here.');
+insert into setting values (default, 'on a revature zoom call, but before the trainer is online.');
+insert into setting values (default, 'at brians house.');
+insert into setting values (default, 'currently on the actual titanic.');
+insert into setting values (default, 'literally in a git hub.');
