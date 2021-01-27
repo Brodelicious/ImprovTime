ba80fdbe3782a9c5ec674e6f0f1958b61b36eb8f
bhufnagel <Brodinator@live.com>
2021-01-27 16:56:23 -0500

    Setting Service implementation

--- frontend/ImprovFront/src/app/services/setting.service.ts
+++ frontend/ImprovFront/src/app/services/setting.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Http2ServerRequest } from 'http2';
+import { Observable } from 'rxjs';
+import {Setting} from '../models/setting';
+import {UrlService} from './url.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SettingService {
+    url: string;
 
-  constructor() { }
+    constructor(private http: HttpClient, private urlServ: UrlService) { 
+        this.url = this.urlServ + "/settings";
+    }
+
+    getSettingById(id: number){
+        return this.http.get(this.url + "/" + id);
+    }
+
+    getAllSettings(){
+        return this.http.get(this.url + "/all");
+    }
+
+    addSetting(setting: Setting){
+        return this.http.post(this.url, setting);
+    }
+
+    updateSetting(setting: Setting){
+        return this.http.put(this.url, setting);
+    }
+
+    deleteSetting(id: number){
+        return this.http.delete(this.url + "/" + id);
+    }
 }
