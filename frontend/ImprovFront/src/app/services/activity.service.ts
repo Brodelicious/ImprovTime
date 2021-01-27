8aa28db00321bfa2f19bbe70ce1495fcf7d58fee
bhufnagel <Brodinator@live.com>
2021-01-27 13:50:33 -0500

    to be stashed

--- frontend/ImprovFront/src/app/services/activity.service.ts
+++ frontend/ImprovFront/src/app/services/activity.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import {Activity} from '../models/activity';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ActivityService {
+    private activitiesUrl: string;
 
-  constructor() { }
+  constructor(private http: HttpClient, private urlService: UrlService) { 
+      this.activitiesUrl = this.urlService.getUrl() + 'activities';
+  }
+
+  getAll(): Observable<Activity[]>{
+      return this.http.get(this.activitiesUrl, {withCredentials:true}).pipe(
+      )
+  }
+
 }
